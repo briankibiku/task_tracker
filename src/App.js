dfd8103609ce86a1061930705d42d572e553cee2
b2k <kibiku.kimani@savannahinformatics.com>
2021-09-21 13:00:17 +0300

    feat: add task function to my task tracker

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import Tasks from './components/Tasks';
-import Header from './components/Header';
-import { useState } from "react"
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([ 
+  ]);
 
-  const [tasks, setTasks] = useState([
-    { id: 1, text: 'Doctor says go ahead', day: 'Fri 13th', reminder: false },
-    { id: 2, text: 'It is a baby girl', day: 'Fri 13th', reminder: true },
-    { id: 3, text: 'Cook and sleep', day: 'Fri 13th', reminder: true },
-    { id: 4, text: 'Rice and beans', day: 'Fri 13th', reminder: false }])
+  // delete/hide task func
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // toggle reminder func
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id
+          ? {
+              ...task,
+              reminder: !task.reminder,
+            }
+          : task
+      )
+    );
+  };
+
+  // add task function
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    console.log(id);
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div className="container">
-      <Tasks tasks = {tasks}/>
-      <Header title = 'Henlo Biks' />
+      <Header
+        title={"Task Tracker"}
+        toggleAddTaskForm={() => setShowAddTask(!showAddTask)}
+          showAddTask={showAddTask}
+      />
+      {/* only positive ternary operator */}
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No task to show"
+      )}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,26 @@
-import PropTypes from 'prop-types' // impt to import this
-import Button from './Button'
+import PropTypes from "prop-types"; // impt to import this
+import Button from "./Button";
 
-// rafce to create a react component using and arrow function 
-const Header = (props) => {
-
-    const onClick = () => {
-        alert('I was clicked');
-        console.log('I have been clicked')
-    }
-    return (
-        <header className='header'>
-            {/* inline css styling */}
-            <h1 style={{ color: 'black', background: 'red' }}>{props.title}</h1>
-            <Button onClick={onClick} color='green' text='Add' />
-            {/* <h1 style={headerStyle}>{props.title}</h1> */}
-
-        </header>
-    )
-}
+// rafce to create a react component using and arrow function
+const Header = ({ title, toggleAddTaskForm, showAddTask }) => {
+  return (
+    <header className="header">
+      {/* inline css styling */}
+      <h1 style={{ color: "black" }}>{title}</h1>
+      <Button
+        onClick={toggleAddTaskForm}
+        color={!showAddTask ? "green" : 'red0'}
+        text={!showAddTask ? "Add" : "Hide"}
+      />
+      {/* <h1 style={headerStyle}>{props.title}</h1> */}
+    </header>
+  );
+};
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
 // css style variable
 // const headerStyle = { color: 'red' }
 
-export default Header
+export default Header;
